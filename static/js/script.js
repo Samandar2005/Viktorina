6485c1f62213ef1fb1e8fad9fc430e0737a3730e
Samandar Bo'riyev <samandarboriyev2005@gmail.com>
2024-11-05 20:16:29 +0500

    Update html file and add css an js files

---
+++ static/js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const submitButtons = document.querySelectorAll('button[type="submit"]');
+    submitButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            button.innerText = "Submitting...";
+            button.disabled = true;
+        });
+    });
+});
